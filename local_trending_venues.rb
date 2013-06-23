c4cfb1a9fb7b84513e182e65f83652166b162436
J Street <rjstreet@newmyths.org>
2013-06-23 16:42:01 -0400

    ''

--- local_trending_venues.rb
+++ local_trending_venues.rb
@@ -8,10 +8,10 @@ class LocalTrendingVenues
     @client = Foursquare2::Client.new(:client_id => '2IWZ2NPWIL1WJO1N4NJBFXBPSCOXCXRUBXAVKRUHPBPBQQ1Z', :client_secret => 'AJSY1VZRLL4PF1GAKKZN0YCR32JKGA2TYBBZKDBCHG32TEFR')
 
     venues = @client.trending_venues('38.052248,-78.513895')
-    if( venues.venues == nil )
+    if( venues.venues.nil? )
       'Nothing trending here!'
     else
-      venues.venues
+      venues.venues.count
     end
   end
 end
