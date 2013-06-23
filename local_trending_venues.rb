fa7931757b478cdfe928c38c7c59d296b9fe8cb2
J Street <rjstreet@newmyths.org>
2013-06-23 16:14:06 -0400

    ''

--- local_trending_venues.rb
+++ local_trending_venues.rb
@@ -7,6 +7,7 @@ class LocalTrendingVenues
   def get_trending_venues
     @client = Foursquare2::Client.new(:client_id => '2IWZ2NPWIL1WJO1N4NJBFXBPSCOXCXRUBXAVKRUHPBPBQQ1Z', :client_secret => 'AJSY1VZRLL4PF1GAKKZN0YCR32JKGA2TYBBZKDBCHG32TEFR')
 
-    @client.trending_venues('38.052248,-78.513895')
+    venues = @client.trending_venues('38.052248,-78.513895')
+    venues.first
   end
 end
